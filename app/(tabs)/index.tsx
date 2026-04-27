7d2202b16989aec1ceeb0d936725cdbb9e0e52b4
Jev-Smith <smithjevaughn20@gmail.com>
2026-04-27 05:57:43 -0500

    Created header for todos tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,28 @@
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, StatusBar } from "react-native";
 import useTheme from "@/hooks/useTheme";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient"
+import Header from "@/components/Header";
 
 export default function Index() {
-  const {toggleDarkMode} = useTheme();
+  const {toggleDarkMode, colors} = useTheme();
+
+  const homeStyles = createHomeStyles(colors);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <Text>Welcome to the app</Text>
+    <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
+      <StatusBar barStyle={colors.statusBarStyle} />
+      
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Header/>
 
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>Toggle Theme</Text>
-      </TouchableOpacity>
-  
-    </View>
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>Toggle Theme</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
